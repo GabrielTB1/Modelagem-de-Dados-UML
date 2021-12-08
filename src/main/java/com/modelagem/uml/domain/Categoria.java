8a41e5434328eb23c0a9168f7c57e5191e22c24c
Gabriel Terres <gabterres@hotmail.com>
2021-12-08 10:42:50 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/modelagem/uml/domain/Categoria.java
+++ src/main/java/com/modelagem/uml/domain/Categoria.java
@@ -2,10 +2,18 @@ package com.modelagem.uml.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
     
     private static final long serialVersionUID = 1L;
     
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
@@ -43,8 +51,6 @@ public class Categoria implements Serializable {
     public boolean equals(Object obj) {
         if (this == obj)
             return true;
-        if (this == null)
-            return false;
         if (getClass() != obj.getClass())
             return false;
         Categoria other = (Categoria) obj;
