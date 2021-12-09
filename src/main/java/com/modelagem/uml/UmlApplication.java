bf27fdd91c9ea078844103a83a239b4d10a330c8
Gabriel Terres <gabterres@hotmail.com>
2021-12-09 09:25:51 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/modelagem/uml/UmlApplication.java
+++ src/main/java/com/modelagem/uml/UmlApplication.java
@@ -3,7 +3,9 @@ package com.modelagem.uml;
 import java.util.Arrays;
 
 import com.modelagem.uml.domain.Categoria;
+import com.modelagem.uml.domain.Produto;
 import com.modelagem.uml.repositories.CategoriaRepository;
+import com.modelagem.uml.repositories.ProdutoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -15,6 +17,8 @@ public class UmlApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(UmlApplication.class, args);
 	}
@@ -24,8 +28,20 @@ public class UmlApplication implements CommandLineRunner {
 		
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+
+		Produto p1 = new Produto(null,"Computador",2000.00);
+		Produto p2 = new Produto(null,"Impressora",800.00);
+		Produto p3 = new Produto(null,"Mouse",80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat1.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
---
+++ src/main/java/com/modelagem/uml/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.modelagem.uml.repositories;
+
+import com.modelagem.uml.domain.Produto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
