9861c98acb8d3167007d639f233270c9c5353d65
Gabriel Terres Bednascki <gabterres@hotmail.com>
2022-03-18 15:00:46 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/modelagem/uml/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.modelagem.uml.repositories;
+
+import com.modelagem.uml.domain.Pagamento;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
---
+++ src/main/java/com/modelagem/uml/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com.modelagem.uml.repositories;
+
+import com.modelagem.uml.domain.Pedido;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
